226037e0decd00b43ea4fd011e1156682daca677
csantiago001 <carlos.santiago2987@gmail.com>
2026-09-17 13:18:58 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-const CACHE_NAME = 'pin-mebuc-v1';
+const CACHE = 'pin-mebuc-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/PIN/index.html',
+  '/PIN/manifest.json',
+  '/PIN/icon-192.png',
+  '/PIN/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
